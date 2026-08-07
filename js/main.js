db34d446dd2148b531c19c5b9b07e6aa7a1e5496
uxxxustudio <uxxxu.studio@gmail.com>
2026-08-07 17:42:49 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-main
+async function loadComponent(id, file) {
+    const res = await fetch(file);
+
+    if (!res.ok) {
+        console.error(file + " load failed");
+        return;
+    }
+
+    document.getElementById(id).innerHTML = await res.text();
+}
+
+window.addEventListener("DOMContentLoaded", async () => {
+
+    await loadComponent("header", "components/header.html");
+
+    await loadComponent("hero", "components/hero.html");
+
+    await loadComponent("service", "components/service.html");
+
+    await loadComponent("portfolio", "components/portfolio.html");
+
+    await loadComponent("about", "components/about.html");
+
+    await loadComponent("contact", "components/contact.html");
+
+    await loadComponent("footer", "components/footer.html");
+
+});
